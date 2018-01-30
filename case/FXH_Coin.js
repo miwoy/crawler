801efe51a395ac0d30ecc1e07bda76583b08baa4
miwoy <guankb@peapad.com.cn>
2018-01-30 16:53:25 +0800

    修改数据源

--- case/FXH_Coin.js
+++ case/FXH_Coin.js
@@ -28,7 +28,37 @@ class FXH_Coin extends Case {
 			blockexplorer: $(".value a", $(secondPark[6])).map((i, el) => $(el).attr("href")).get(),
 			publish_time: $(".value", secondPark[3]).text(),
 			book_url: $($(".value a", secondPark[4])).attr("href"),
-			type: $(".value", $(secondPark[7])).text().replace("\n", "").trim() === "是" ? 2 : 1
+			type: $(".value", $(secondPark[7])).text().replace("\n", "").trim() === "是" ? 2 : 1,
+			total_amount: parseFloat($("#baseInfo > div.firstPart > div:nth-child(3) > div:nth-child(4)").text().replace(/,/g, "")),
+			attach: {
+				total_market_cap_usd: parseFloat(($("#baseInfo > div.firstPart > div:nth-child(2) > div:nth-child(3)").text().split("$")[1] || "").replace(/,/g, "")),
+				amount: parseFloat($("#baseInfo > div.firstPart > div:nth-child(3) > div:nth-child(2)").text().replace(/,/g, "")),
+				price_usd: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, "")),
+				min_price_usd: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, "")),
+				max_price_usd: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, "")),
+
+				// statistic
+				min_price_usd_statistic: {
+					of24h: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, "")),
+					of7d: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, "")),
+					of30d: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, ""))
+				},
+				max_price_usd_statistic: {
+					of24h: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, "")),
+					of7d: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, "")),
+					of30d: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, ""))
+				},
+				volume_usd_statistic: {
+					of24h: parseFloat(($("#baseInfo > div.firstPart > div:nth-child(4) > div:nth-child(3)").text().split("$")[1] || "").replace(/,/g, "")),
+					of7d: 0,
+					of30d: 0
+				},
+				open_price_usd_statistic: {
+					of24h: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, "")),
+					of7d: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, "")),
+					of30d: parseFloat(($("#baseInfo > div.firstPart > div.cell.maket > div.sub > span:nth-child(1)").text().split("$")[1] || "").replace(/,/g, ""))
+				}
+			}
 
 		};
 
@@ -38,7 +68,12 @@ class FXH_Coin extends Case {
 			path: evidence.desc
 		}));
 
-		evidence.desc = await fxh_coin_desc.start();
+		try {
+			evidence.desc = await fxh_coin_desc.start();
+		} catch (e) {
+			evidence.desc = ""
+		}
+		
 
 		return evidence;
 
@@ -46,39 +81,39 @@ class FXH_Coin extends Case {
 	async criminate(evidence, intell) {
 		// impl
 		// create
-		let dataurl = "https://api.coinmarketcap.com/v1/ticker";
-		let url = dataurl + intell.path.split("/currencies").pop();
-		debug("request: " + url)
-		let data = await request.get(url);
-		if (!data[0]) return;
-		data = data[0];
-		evidence = _.assign(evidence, {
-			cmc_id: data.id,
-			en_name: data.name,
-			symbol: data.symbol,
-			total_amount: data.max_supply,
-			attach: {
-				total_market_cap_usd: data.market_cap_usd,
-				amount: data.total_supply,
-				price_usd: data.price_usd,
-				min_price_usd: data.price_usd,
-				max_price_usd: data.price_usd,
+		// let dataurl = "https://api.coinmarketcap.com/v1/ticker";
+		// let url = dataurl + intell.path.split("/currencies").pop();
+		// debug("request: " + url)
+		// let data = await request.get(url);
+		// if (!data[0]) return;
+		// data = data[0];
+		// evidence = _.assign(evidence, {
+		// 	cmc_id: data.id,
+		// 	en_name: data.name,
+		// 	symbol: data.symbol,
+		// 	total_amount: data.max_supply,
+		// 	attach: {
+		// 		total_market_cap_usd: data.market_cap_usd,
+		// 		amount: data.total_supply,
+		// 		price_usd: data.price_usd,
+		// 		min_price_usd: data.price_usd,
+		// 		max_price_usd: data.price_usd,
 
-				// statistic
-				min_price_usd_statistic: {},
-				max_price_usd_statistic: {},
-				price_usd_statistic: {
-					of24h: data.price_usd
-				},
-				volume_usd_statistic: {
-					of24h: data["24h_volume_usd"]
-				},
-				percent_change_statistic: {
-					of24h: data["percent_change_24h"],
-					of7d: data["percent_change_7d"]
-				}
-			}
-		})
+		// 		// statistic
+		// 		min_price_usd_statistic: {},
+		// 		max_price_usd_statistic: {},
+		// 		price_usd_statistic: {
+		// 			of24h: data.price_usd
+		// 		},
+		// 		volume_usd_statistic: {
+		// 			of24h: data["24h_volume_usd"]
+		// 		},
+		// 		percent_change_statistic: {
+		// 			of24h: data["percent_change_24h"],
+		// 			of7d: data["percent_change_7d"]
+		// 		}
+		// 	}
+		// })
 
 		// update
 
